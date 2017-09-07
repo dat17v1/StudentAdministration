7a8952603ad1553c57a64bdfc6dbbe01bd01b016
clbokea <clbo@kea.dk>
2017-09-07 12:58:39 +0200

    intital class show

---
+++ src/main/java/clbo/controller/StudentController.java
@@ -0,0 +1,28 @@
+package clbo.controller;
+
+import clbo.model.entities.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by clbo on 07/09/2017.
+ */
+
+@Controller
+public class StudentController {
+
+    ArrayList<Student> students = new ArrayList<Student>();
+
+    @GetMapping("/")
+    public String index(Model model) {
+
+        students.add(new Student("1", "Claus", "Bove", new Date(2010, 12, 01), "221070-9999"));
+
+        model.addAttribute("stu", students);
+        return "index";
+    }
+}
---
+++ src/main/java/clbo/model/entities/Student.java
@@ -0,0 +1,62 @@
+package clbo.model.entities;
+
+import java.util.Date;
+
+/**
+ * Created by clbo on 07/09/2017.
+ */
+public class Student {
+    private String studentId;
+    private String firstName;
+    private String lastName;
+    private Date enrollmentDate;
+    private String cpr;
+
+    public Student(String studentId, String firstName, String lastName, Date enrollmentDate, String cpr) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.enrollmentDate = enrollmentDate;
+        this.cpr = cpr;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(Date enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public String getCpr() {
+        return cpr;
+    }
+
+    public void setCpr(String cpr) {
+        this.cpr = cpr;
+    }
+}
